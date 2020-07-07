2fe7e102ab1e29ec832d94e2272545fa6e9c095b
SVO <sean.vanosselaer@myob.com>
2020-07-07 21:24:54 +1000

    Getting to green end-to-end tests for new use case

--- spec/end_to_end_tests/pageObjects/radar_page.js
+++ spec/end_to_end_tests/pageObjects/radar_page.js
@@ -3,7 +3,6 @@ class RadarPage {
     this.blip = '.quadrant-group-second .blip-link'
     this.blip_selected = '.quadrant-table.selected .blip-list-item'
     this.blip_description = '.blip-item-description.expanded p'
-    this.sheet2 = '.alternative'
     this.autocomplete = '.search-radar'
     this.search_value = 'Babel'
     this.search_item = '.ui-menu-item:first'
@@ -19,10 +18,6 @@ class RadarPage {
 
   validateBlipDescription (text) {
     expect(cy.get(this.blip_description).contains(text))
-  }
-
-  clickSheet2 () {
-    cy.get(this.sheet2).click()
   }
 
   searchTheBlip () {
